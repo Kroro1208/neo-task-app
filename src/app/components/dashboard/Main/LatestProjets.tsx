6f192f5e0f384b27c2d05381c7c497ac97f5f143
Kroro1208 <yamagata_7580@yahoo.co.jp>
2024-06-12 08:10:07 +0900

    微修正

--- src/app/components/dashboard/Main/LatestProjets.tsx
+++ src/app/components/dashboard/Main/LatestProjets.tsx
@@ -31,23 +31,23 @@ const LatestProjets = () => {
                     height={10}
                 />
                 <span>Project 1</span>
-                {/* Progress */}
-                <div className='flex flex-col gap-2'>
-                    <div className={`${isDark ? "text-white" : "text-gray-500"}
-                        flex justify-between items-center text-[12px]`}>
-                        <div className='flex gap-2 items-center'>
-                            <FontAwesomeIcon
-                                icon={faBarsProgress}
-                                width={12}
-                                height={12}
-                            />
-                            <span>Progress</span>
-                        </div>
-                        <span>9/12</span>
+            </div>
+            {/* Progress */}
+            <div className='flex flex-col gap-2'>
+                <div className={`${isDark ? "text-white" : "text-gray-500"}
+                    flex justify-between items-center text-[12px]`}>
+                    <div className='flex gap-2 items-center'>
+                        <FontAwesomeIcon
+                            icon={faBarsProgress}
+                            width={12}
+                            height={12}
+                        />
+                        <span>Progress</span>
                     </div>
-                    <div className='w-full rounded-2xl h-[5px] bg-gray-400 overflow-hidden'>
-                        <div className='w-1/2 h-full bg-mainColor rounded-r-lg'></div>
-                    </div>
+                    <span>9/12</span>
+                </div>
+                <div className='w-full rounded-2xl h-[5px] bg-gray-400 overflow-hidden'>
+                    <div className='w-1/2 h-full bg-mainColor rounded-r-lg'></div>
                 </div>
             </div>
         </div>
