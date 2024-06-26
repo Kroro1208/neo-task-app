27fe1853452bc67d4e4bef4a5996d005efef46a5
Kroro1208 <yamagata_7580@yahoo.co.jp>
2024-06-27 08:32:10 +0900

    微修正

--- src/app/components/CategoriesScreen/CategoriesArea.tsx
+++ src/app/components/CategoriesScreen/CategoriesArea.tsx
@@ -1,3 +1,4 @@
+"use client"
 import { useGlobalContextProvider } from '@/app/ContextAPI';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React, { useEffect, useState } from 'react'
